acf1838a80d792fedde32f04473de003091e0a7e
Leonid Rozenblyum <lrozenblyum@gmail.com>
2018-02-20 21:16:08 +0200

    First proof of concept - successful parsing of the html page.
    Sum to pay is correctly found.

--- src/main/java/com/leokom/publicutility/bill/Bill.java
+++ src/main/java/com/leokom/publicutility/bill/Bill.java
@@ -1,10 +1,22 @@
 package com.leokom.publicutility.bill;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class Bill {
+	private final String serverResponse;
+
 	public Bill( String serverResponse ) {
+		this.serverResponse = serverResponse;
 	}
 
 	public String toPay() {
-		return "10.18";
+		Document response = Jsoup.parse( serverResponse );
+		
+		return response.getElementsContainingText("Сума до оплати").stream().
+		filter( element -> element.is("TD") )
+		.map( element -> element.nextElementSibling() )
+		.map( element -> element.text() )
+		.findFirst().orElseThrow(() -> new IllegalArgumentException(" Failed to find what to pay from " + serverResponse ) );
 	}
 }
