9190d3fdf763b82fa07ade4ebbd898cf23ad6e3e
Leonid Rozenblyum <lrozenblyum@gmail.com>
2018-02-20 21:57:22 +0200

    Refactoring & comments

--- src/main/java/com/leokom/publicutility/bill/Bill.java
+++ src/main/java/com/leokom/publicutility/bill/Bill.java
@@ -2,6 +2,7 @@ package com.leokom.publicutility.bill;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class Bill {
 	private final String serverResponse;
@@ -11,12 +12,18 @@ public class Bill {
 	}
 
 	public String toPay() {
+		//technically jsoup can also send a request to the server
 		Document response = Jsoup.parse( serverResponse );
-		
-		return response.getElementsContainingText("Сума до оплати").stream().
-		filter( element -> element.is("TD") )
-		.map( element -> element.nextElementSibling() )
-		.map( element -> element.text() )
-		.findFirst().orElseThrow(() -> new IllegalArgumentException(" Failed to find what to pay from " + serverResponse ) );
+
+		return 
+			response
+			.getElementsContainingText("Сума до оплати")
+			.stream()
+			//searching for the next TD after "Сума до оплати"
+			.filter(element -> element.is("TD"))
+			.map(Element::nextElementSibling)
+			.map(Element::text)
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException(" Failed to find what to pay from " + serverResponse));
 	}
 }
