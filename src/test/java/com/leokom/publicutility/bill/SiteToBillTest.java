a79a3dab8f99585b8d6857bca0f73ce79fdf278c
Leonid Rozenblyum <lrozenblyum@gmail.com>
2018-03-01 22:06:07 +0200

    Fixed a lot of qulice remarks.
    Now we have 208 violations.

--- src/test/java/com/leokom/publicutility/bill/SiteToBillTest.java
+++ src/test/java/com/leokom/publicutility/bill/SiteToBillTest.java
@@ -9,24 +9,21 @@ import org.cactoos.text.TextOf;
 import org.junit.Test;
 
 public class SiteToBillTest {
-	@Test
-	public void singleFieldParsed() throws IOException {
-		String serverResponse = loadFile( "SampleResponse.html" );
-		
-		assertEquals( "10.18", new Bill( serverResponse ).toPay() );
-	}
-	
-	@Test
-	public void singleFileParsedTriangulate() throws IOException {
-		String serverResponse = loadFile( "SampleResponseAnotherPayment.html" );
-		
-		assertEquals( "988.17", new Bill( serverResponse ).toPay() );
-	}
+    @Test
+    public void singleFieldParsed() throws IOException {
+        String serverResponse = loadFile("SampleResponse.html");
 
-	private String loadFile( String path ) throws IOException {
-		return 
-			new TextOf( 
-				new ResourceOf( path ) 
-			).asString();
-	}
+        assertEquals("10.18", new Bill(serverResponse).toPay());
+    }
+
+    @Test
+    public void singleFileParsedTriangulate() throws IOException {
+        String serverResponse = loadFile("SampleResponseAnotherPayment.html");
+
+        assertEquals("988.17", new Bill(serverResponse).toPay());
+    }
+
+    private String loadFile(String path) throws IOException {
+        return new TextOf(new ResourceOf(path)).asString();
+    }
 }
