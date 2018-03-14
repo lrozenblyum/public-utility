498c6fdcc0bf1bb1800af67ca9d72dd83dfa2fd7
Leonid Rozenblyum <lrozenblyum@gmail.com>
2018-03-14 22:53:46 +0200

    License statement added to all required classes.

--- src/main/java/com/leokom/publicutility/bill/package-info.java
+++ src/main/java/com/leokom/publicutility/bill/package-info.java
@@ -1,5 +1,6 @@
-/* 
+/**
  * Copyright 5778 Leonid Rozenblium
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,8 +14,6 @@
  */
 
 /**
- * 
- * 
  * Bill-related.
  */
 package com.leokom.publicutility.bill;
