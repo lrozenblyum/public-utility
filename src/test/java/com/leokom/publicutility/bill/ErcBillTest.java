eb4bc7088234c350699a9fc883fccf97cc7124fc
Leonid Rozenblyum <lrozenblyum@gmail.com>
2018-03-18 22:08:52 +0200

    Several qulice remarks fixed

--- src/test/java/com/leokom/publicutility/bill/ErcBillTest.java
+++ src/test/java/com/leokom/publicutility/bill/ErcBillTest.java
@@ -15,25 +15,31 @@
 
 package com.leokom.publicutility.bill;
 
-import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import org.cactoos.io.ResourceOf;
 import org.cactoos.text.TextOf;
+import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * @author Leonid Rozenblium (lrozenblyum@gmail.com)
+ * @version $Id$
+ * @since 0.0.1
+ *
+ */
 public class ErcBillTest {
     @Test
     public void singleFieldParsed() throws IOException {
         String serverResponse = this.loadFile("SampleResponse.html");
 
-        assertEquals("10.18", new ErcBill(serverResponse).toPay());
+        Assert.assertEquals("10.18", new ErcBill(serverResponse).toPay());
     }
 
     @Test
     public void singleFileParsedTriangulate() throws IOException {
         String serverResponse = this.loadFile("SampleResponseAnotherPayment.html");
 
-        assertEquals("988.17", new ErcBill(serverResponse).toPay());
+        Assert.assertEquals("988.17", new ErcBill(serverResponse).toPay());
     }
 
     private String loadFile(String path) throws IOException {
