aee9ed22b229b843c95b2da77b9ebc46990f3e77
Leonid Rozenblyum <lrozenblyum@gmail.com>
2018-05-08 22:39:00 +0300

    Red test: need to parse the real website

--- src/test/java/com/leokom/publicutility/bill/ErcWebSiteTest.java
+++ src/test/java/com/leokom/publicutility/bill/ErcWebSiteTest.java
@@ -9,6 +9,11 @@ public class ErcWebSiteTest {
     @Test
     public void basicWebSite() {
         int account=45011;
-        assertThat( new ErcWebSite(account).load(), CoreMatchers.containsString("45011"));
+        assertThat(new ErcWebSite(account).load(), CoreMatchers.containsString("45011"));
+    }
+    
+    @Test
+    public void webSiteContainsCorrectAddress() {
+        assertThat(new ErcWebSite(21805).load(), CoreMatchers.containsString("Миколайчука Iвана"));
     }
 }
