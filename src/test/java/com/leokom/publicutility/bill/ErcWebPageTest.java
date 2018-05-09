3128d49c9fa26c4e8d75eda5c26f459b0621e20a
Leonid Rozenblyum <lrozenblyum@gmail.com>
2018-05-09 22:22:46 +0300

    Some qulice remarks fixed

--- src/test/java/com/leokom/publicutility/bill/ErcWebPageTest.java
+++ src/test/java/com/leokom/publicutility/bill/ErcWebPageTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 5778 Leonid Rozenblium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.leokom.publicutility.bill;
 
 import static org.junit.Assert.assertThat;
@@ -8,16 +23,23 @@ import org.junit.Test;
 /**
  * Test the web page.
  * Technically it should be some kind of an Integrational Test (since it can be red while the feature is working fine).
- * @author leokom
  *
  */
 public class ErcWebPageTest {
+    /**
+     * Load the web-site and verify response matching the account id in the request.
+     * @throws IOException in case the web site is not reachable
+     */
     @Test
     public void basicWebSite() throws IOException {
         int account=21820;
         assertThat(new ErcWebPage(account).asString(), CoreMatchers.containsString("21820"));
     }
-    
+
+    /**
+     * Load the web-site and verify response contains a correct address.
+     * @throws IOException
+     */
     @Test
     public void webSiteContainsCorrectAddress() throws IOException {
         assertThat(new ErcWebPage(21805).asString(), CoreMatchers.containsString("Миколайчука Iвана"));
