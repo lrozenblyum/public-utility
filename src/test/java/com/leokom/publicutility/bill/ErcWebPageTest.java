8c8fd797c1a4de5da511bb0694cc08b3cd858772
Leonid Rozenblyum <lrozenblyum@gmail.com>
2018-08-01 22:21:42 +0300

    Fixed part of qulice remarks

--- src/test/java/com/leokom/publicutility/bill/ErcWebPageTest.java
+++ src/test/java/com/leokom/publicutility/bill/ErcWebPageTest.java
@@ -15,33 +15,40 @@
 
 package com.leokom.publicutility.bill;
 
-import static org.junit.Assert.assertThat;
 import java.io.IOException;
 import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
 /**
  * Test the web page.
- * Technically it should be some kind of an Integrational Test (since it can be red while the feature is working fine).
- *
+ * Technically it should be some kind of an Integrational Test
+ * (since it can be red while the feature is working fine).
+ * @since 0.0.1
  */
-public class ErcWebPageTest {
+public final class ErcWebPageTest {
     /**
-     * Load the web-site and verify response matching the account id in the request.
+     * Load the web-site and verify response
+     *  matching the account id in the request.
      * @throws IOException in case the web site is not reachable
      */
     @Test
     public void basicWebSite() throws IOException {
-        int account=21820;
-        assertThat(new ErcWebPage(account).asString(), CoreMatchers.containsString("21820"));
+        MatcherAssert.assertThat(
+            new ErcWebPage(21820).asString(),
+            CoreMatchers.containsString("21820")
+        );
     }
 
     /**
      * Load the web-site and verify response contains a correct address.
-     * @throws IOException
+     * @throws IOException in case the web site is not reachable
      */
     @Test
     public void webSiteContainsCorrectAddress() throws IOException {
-        assertThat(new ErcWebPage(21805).asString(), CoreMatchers.containsString("Миколайчука Iвана"));
+        MatcherAssert.assertThat(
+            new ErcWebPage(21805).asString(), 
+            CoreMatchers.containsString("Миколайчука Iвана")
+        );
     }
 }
